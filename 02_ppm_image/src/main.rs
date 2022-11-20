4f7cd0e155301f413313a12c1ade04c308a69cfc
Tian Yu <gasnus@gmail.com>
2022-11-20 19:19:24 +0100

    Output an image

---
+++ 02_ppm_image/src/main.rs
@@ -0,0 +1,25 @@
+use std::io::{self, Write};
+
+const IMAGE_WIDTH: usize = 256;
+const IMAGE_HEIGHT: usize = 256;
+
+fn main() {
+    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+        io::stderr().flush().unwrap();
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+    eprintln!("\nDone.");
+}
