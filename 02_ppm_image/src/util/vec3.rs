32bc4e43f79b6db9151800ae01cc2d9b1de780d5
Tian Yu <gasnus@gmail.com>
2022-11-21 19:15:34 +0100

    Use macro to simplify the code

--- 02_ppm_image/src/util/vec3.rs
+++ 02_ppm_image/src/util/vec3.rs
@@ -1,35 +1,55 @@
-use std::ops::{AddAssign, DivAssign, MulAssign, Neg};
+use std::{
+    fmt::Display,
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign},
+};
 
 // use Vec3 as Point3;
 // use Vec3 as Color;
 
-struct Vec3 {
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Vec3 {
     e: [f64; 3],
 }
 
 impl Vec3 {
-    fn new(e0: f64, e1: f64, e2: f64) -> Self {
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
         Self { e: [e0, e1, e2] }
     }
 
-    fn x(&self) -> f64 {
+    pub fn x(&self) -> f64 {
         self.e[0]
     }
 
-    fn y(&self) -> f64 {
+    pub fn y(&self) -> f64 {
         self.e[1]
     }
 
-    fn z(&self) -> f64 {
+    pub fn z(&self) -> f64 {
         self.e[2]
     }
 
-    fn length(&self) -> f64 {
+    pub fn length(&self) -> f64 {
         self.length_squared().sqrt()
     }
 
-    fn length_squared(&self) -> f64 {
+    pub fn length_squared(&self) -> f64 {
         self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
+
+    pub fn dot(&self, other: &Vec3) -> f64 {
+        self.e[0] * other.e[0] + self.e[1] * other.e[1] + self.e[2] * other.e[2]
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3::new(
+            self.e[1] * other.e[2] - self.e[2] * other.e[1],
+            self.e[2] * other.e[0] - self.e[0] * other.e[2],
+            self.e[0] * other.e[1] - self.e[1] * other.e[0],
+        )
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        self / self.length()
     }
 }
 
@@ -39,35 +59,139 @@ impl Default for Vec3 {
     }
 }
 
-impl Neg for Vec3 {
-    type Output = Self;
-
-    fn neg(self) -> Self::Output {
-        Vec3::new(-self.e[0], -self.e[1], -self.e[2])
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.e[0], self.e[1], self.e[2])
     }
 }
 
-impl AddAssign for Vec3 {
-    fn add_assign(&mut self, rhs: Self) {
-        self.e[0] += rhs.e[0];
-        self.e[1] += rhs.e[1];
-        self.e[2] += rhs.e[2];
-    }
+macro_rules! gen_binary_ops {
+    ($Vector:ident $op:ident $func:ident $symbol:tt) => {
+        impl $op for &$Vector {
+            type Output = $Vector;
+
+            fn $func(self, other: &$Vector) -> $Vector {
+                $Vector::new(self.e[0] $symbol other.e[0], self.e[1] $symbol other.e[1], self.e[2] $symbol other.e[2])
+            }
+        }
+
+        impl $op<$Vector> for $Vector {
+            type Output = $Vector;
+
+            fn $func(self, other: $Vector) -> $Vector {
+                &self $symbol &other
+            }
+        }
+
+        impl $op<& $Vector> for $Vector {
+            type Output = $Vector;
+
+            fn $func(self, other: & $Vector) -> $Vector {
+                &self $symbol other
+            }
+        }
+
+        impl $op<$Vector> for & $Vector {
+            type Output = $Vector;
+
+            fn $func(self, other: $Vector) -> $Vector {
+                self $symbol &other
+            }
+        }
+
+        impl $op<f64> for & $Vector {
+            type Output = $Vector;
+
+            fn $func(self, other: f64) -> $Vector {
+                $Vector::new(self.e[0] $symbol other, self.e[1] $symbol other, self.e[2] $symbol other)
+            }
+        }
+
+        impl $op<f64> for $Vector {
+            type Output = $Vector;
+
+            fn $func(self, other: f64) -> $Vector {
+                &self $symbol other
+            }
+        }
+
+        impl $op<$Vector> for f64 {
+            type Output = $Vector;
+
+            fn $func(self, other: $Vector) -> $Vector {
+                &other $symbol self
+            }
+        }
+
+        impl $op<&$Vector> for f64 {
+            type Output = $Vector;
+
+            fn $func(self, other: &$Vector) -> $Vector {
+                other $symbol self
+            }
+        }
+    };
 }
 
-impl MulAssign<f64> for Vec3 {
-    fn mul_assign(&mut self, rhs: f64) {
-        self.e[0] *= rhs;
-        self.e[1] *= rhs;
-        self.e[2] *= rhs;
-    }
+macro_rules! gen_unary_ops {
+    ($Vector:ident $op:ident $func:ident $symbol:tt) => {
+        impl $op for & $Vector {
+            type Output = $Vector;
+
+            fn $func(self) -> $Vector {
+                $Vector::new($symbol self.e[0], $symbol self.e[1], $symbol self.e[2])
+            }
+        }
+
+        impl $op for $Vector {
+            type Output = $Vector;
+
+            fn $func(self) -> $Vector {
+                $symbol &self
+            }
+        }
+    };
 }
 
-impl DivAssign<f64> for Vec3 {
-    fn div_assign(&mut self, rhs: f64) {
-        *self *= 1.0 / rhs;
-    }
+macro_rules! gen_op_assign {
+    ($Vector:ident $op:ident $func:ident $symbol:tt) => {
+        impl $op<& $Vector> for $Vector {
+            fn $func(&mut self, other: & $Vector) {
+                self.e[0] $symbol other.e[0];
+                self.e[1] $symbol other.e[1];
+                self.e[2] $symbol other.e[2];
+            }
+        }
+
+        impl $op for $Vector {
+            fn $func(&mut self, other: $Vector) {
+                self.e[0] $symbol &other.e[0];
+                self.e[1] $symbol &other.e[1];
+                self.e[2] $symbol &other.e[2];
+            }
+        }
+
+        impl $op<f64> for $Vector {
+            fn $func(&mut self, other: f64) {
+                self.e[0] $symbol other;
+                self.e[1] $symbol other;
+                self.e[2] $symbol other;
+            }
+        }
+    };
 }
+
+gen_binary_ops!(Vec3 Add add +);
+gen_binary_ops!(Vec3 Sub sub -);
+gen_binary_ops!(Vec3 Mul mul *);
+gen_binary_ops!(Vec3 Div div /);
+
+gen_unary_ops!(Vec3 Neg neg -);
+
+gen_op_assign!(Vec3 AddAssign add_assign +=);
+gen_op_assign!(Vec3 SubAssign sub_assign -=);
+gen_op_assign!(Vec3 MulAssign mul_assign *=);
+gen_op_assign!(Vec3 DivAssign div_assign /=);
 
 #[cfg(test)]
 mod tests {
@@ -99,7 +223,6 @@ mod tests {
         assert_eq!(-2.0, a.y());
         assert_eq!(-3.0, a.z());
     }
-
 
     #[test]
     fn test_vec3_add_vec3() {
