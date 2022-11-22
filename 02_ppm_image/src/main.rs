80ffc2eed571b69efe20728073b24a804087dbbf
Tian Yu <gasnus@gmail.com>
2022-11-22 21:15:26 +0100

    ray-sphere intersection

--- 02_ppm_image/src/main.rs
+++ 02_ppm_image/src/main.rs
@@ -46,8 +46,22 @@ fn main() {
 }
 
 fn ray_color(r: &Ray) -> Vec3 {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = r.dir().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
 
     (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.dir().dot(r.dir());
+    let b = 2.0 * oc.dot(r.dir());
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant > 0.0
 }
