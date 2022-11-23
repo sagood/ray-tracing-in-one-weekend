2a55111f3d6b562e816ed1919362ade25b3f71d4
Tian Yu <gasnus@gmail.com>
2022-11-23 14:51:32 +0100

    Simplify the ray-sphere intersection code

--- 02_ppm_image/src/main.rs
+++ 02_ppm_image/src/main.rs
@@ -60,13 +60,13 @@ fn ray_color(r: &Ray) -> Vec3 {
 
 fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
-    let a = r.dir().dot(r.dir());
-    let b = 2.0 * oc.dot(r.dir());
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let a = r.dir().length_squared();
+    let half_b = oc.dot(r.dir());
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
     if discriminant < 0.0 {
         -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (-half_b - discriminant.sqrt()) / a
     }
 }
