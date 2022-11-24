261f19697e9c78a8ee734f6f27d3651f79d4bb93
Tian Yu <gasnus@gmail.com>
2022-11-24 11:35:40 +0100

    fuzzed metal

--- 02_ppm_image/src/main.rs
+++ 02_ppm_image/src/main.rs
@@ -33,8 +33,8 @@ fn main() {
 
     let material_ground = Arc::new(Lambertian::new(&Vec3::new(0.8, 0.8, 0.0)));
     let material_center = Arc::new(Lambertian::new(&Vec3::new(0.7, 0.3, 0.3)));
-    let material_left = Arc::new(Metal::new(&Vec3::new(0.8, 0.8, 0.8)));
-    let material_right = Arc::new(Metal::new(&Vec3::new(0.8, 0.6, 0.2)));
+    let material_left = Arc::new(Metal::new(&Vec3::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Arc::new(Metal::new(&Vec3::new(0.8, 0.6, 0.2), 1.0));
 
     world.add(Arc::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
