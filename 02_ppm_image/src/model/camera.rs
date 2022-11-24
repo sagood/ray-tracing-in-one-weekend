1a8f69114fda24986365266ed9dd7daa3d769ec0
Tian Yu <gasnus@gmail.com>
2022-11-24 14:50:53 +0100

    A distant view

--- 02_ppm_image/src/model/camera.rs
+++ 02_ppm_image/src/model/camera.rs
@@ -12,31 +12,38 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(vfov: f64, aspect_ratio: f64) -> Self {
+    pub fn new(
+        lookfrom: &Point3,
+        lookat: &Point3,
+        vup: &Vec3,
+        vfov: f64,
+        aspect_ratio: f64,
+    ) -> Self {
         let theta = degrees_to_radians(vfov);
         let h = (theta / 2.0).tan();
         let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
 
-        let focal_length = 1.0;
-        let origin = Point3::new(0.0, 0.0, 0.0);
-        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let w = (lookfrom - lookat).unit_vector();
+        let u = (vup.cross(&w)).unit_vector();
+        let v = w.cross(&u);
+
+        let origin = lookfrom.clone();
+        let horizontal = viewport_width * u;
+        let vertical = viewport_height * v;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - w;
         Self {
             origin,
             horizontal,
             vertical,
-            lower_left_corner: origin
-                - horizontal / 2.0
-                - vertical / 2.0
-                - Vec3::new(0.0, 0.0, focal_length),
+            lower_left_corner,
         }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         Ray::new(
             &self.origin,
-            &(self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin),
+            &(self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin),
         )
     }
 }
